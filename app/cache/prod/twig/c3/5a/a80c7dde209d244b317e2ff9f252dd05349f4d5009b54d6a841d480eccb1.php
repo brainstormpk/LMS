21165a529dd5fddb467511f219862375321028b6
Brain Storm <brainstormpk.hr@gmail.com>
2014-12-27 17:04:21 +0500

    LMS complete project files in Symfony 2.6.1 has been committed on 12/27/2014 5:04 PM

---
+++ app/cache/prod/twig/c3/5a/a80c7dde209d244b317e2ff9f252dd05349f4d5009b54d6a841d480eccb1.php
@@ -0,0 +1,49 @@
+<?php
+
+/* default/index.html.twig */
+class __TwigTemplate_c35aa80c7dde209d244b317e2ff9f252dd05349f4d5009b54d6a841d480eccb1 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("base.html.twig");
+
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        // line 4
+        echo "    Homepage.
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "default/index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 4,  28 => 3,);
+    }
+}
